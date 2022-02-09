16c7704b06e2a1f5b57db0738998e3b04cf1dfb0
Nathan Kennedy <93215200+Nathan632pm@users.noreply.github.com>
2022-02-09 18:23:19 -0500

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
-import Header from './Header.jsx';
+import Header from "./Header.jsx";
 
 const App = () => {
+  const [callData, setCallData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    fetch("https://aircall-job.herokuapp.com/activities")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setCallData(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  };
+
+  const postData = (id, isArchived) => {
+    fetch("https://aircall-job.herokuapp.com/activities/:" + id, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: {
+        is_archived: isArchived,
+      },
+    });
+  };
+
   return (
-    <div className='container'>
-      <Header/>
-      <div className="container-view">Some activities should be here</div>
+    <div className="container">
+      <Header />
+      {!isLoaded ? (
+        <div className="container-view">Loading...</div>
+      ) : (
+        <div className="container-view">Some activities should be here</div>
+      )}
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
